689efc49d00b3c0f1f16a9c277709d03ecd39085
Barak <barozbarak@gmail.com>
2022-11-07 00:02:45 +0200

    components hierarchy including css without ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
 import logo from './logo.svg';
-import './App.css';
+import './coin.css';
+import React from 'react';
 
-function App() {
+
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SearchableCoinCrypitoList coins={props.coins}/>
   );
 }
+
+
+
+
+class Percentage extends React.Component{
+  render(){ 
+    const precent=this.props.precent;
+     if (precent<0)
+     {
+     return  <h5 className='red-precent coin-precent'> {precent} %</h5>
+     }
+     else{
+      return <h5 className='green-precent coin-precent'> {precent} %</h5>
+     }
+    }
+     
+}
+
+
+
+class CoinRow extends React.Component{
+  render(){
+    const coin=this.props.coin;
+    const src= coin.src;
+
+    return(
+      <div className='coin-row'>
+         <div className='coin-id'>
+             <img src={src}  className='coin-img'/>
+             <h1>{coin.name}</h1>
+         </div>
+         <div className='coin-data'>
+             
+             <h5 >{coin.price}</h5>
+             <Percentage className='coin-precent' precent={coin.precent}/>
+             <h5 className='coin-marketcap'>{coin.marketcap}</h5>
+         </div>
+      </div>
+    )
+  }
+}
+class CoinCrypitoList extends React.Component{
+  render(){
+     let rows=[];
+      this.props.coins.forEach(coin => {
+        rows.push(<CoinRow 
+                   coin={coin}
+                   key={coin.name}
+                   />)
+      })
+    return(
+     <div className='coin-con'>
+     {rows}
+     </div>
+    )
+  }
+}
+
+
+
+class SearchBar extends React.Component{
+  render(){
+    return(
+      <div className='coin-search'>
+        <form>
+           <input className='coin-input' type="text" placeholder="Provide the coin name"/>
+        </form>  
+      </div>
+    )
+  }
+}
+
+
+class SearchableCoinCrypitoList extends React.Component{
+  render(){
+    return(
+      <div className='top-app  '>
+        <SearchBar coins={this.props.coins}/>
+        <CoinCrypitoList coins={this.props.coins}/>
+      </div>
+   )
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Bitcoin from './pictures/b.webp';
+import bnb from './pictures/c.webp';
+import ethereum  from'./pictures/e.webp';
+import tether from './pictures/t.webp';
+
+
+
+
+
+ const  COINS=[{name:'Bitcoain',price: 'Rs.1343433',precent: -0.75,marketcap:'Rs 44,433,322,323' ,src:Bitcoin},
+       {name:'Ethereum',price: 'Rs.1343433',precent: 0.75,marketcap:'Rs 44,433,322,323',src:ethereum},
+       {name:'Tether',price: 'Rs.1343433',precent: 2,marketcap:'Rs 44,433,322,323',src:tether},
+       {name:'BNB',price: 'Rs.1343433',precent: -1,marketcap:'Rs 44,433,322,323',src:bnb},
+
+      ]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App coins={COINS} />
   </React.StrictMode>
 );
 
+
+  
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
